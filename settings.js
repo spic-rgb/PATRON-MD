8608ff26a40a76e2d107bc0a395caf4879b53c25
spic-rgb <chassupremo@gmail.com>
2025-04-08 16:00:40 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ global.bankowner = "FADARE"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
-global.wlcm = true
+global.wlcm = false
 global.autoblockmorroco = true //auto block 212 (true to on, false to off)
 global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
 global.antispam = false//auto kick spammer (true to on, false to off)
